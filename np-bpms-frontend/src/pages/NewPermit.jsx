d0fd47b92b41a850f67991a47f8f3928efefbcb2
CG <cagegbeku@gmail.com>
2026-07-17 13:16:04 +0000

    Automated backup

--- np-bpms-frontend/src/pages/NewPermit.jsx
+++ np-bpms-frontend/src/pages/NewPermit.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import DocumentScanner from './DocumentScanner';
+import DocumentScanner from '../DocumentScanner';
 
 const NewPermit = () => {
   const [formData, setFormData] = useState({
